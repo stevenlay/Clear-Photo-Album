0eef81279c92bbc98840ee742011965dd61f28c9
Steven Lay <stlay@ucsd.edu>
2017-11-18 21:21:04 -0800

    Imgur and Selenium integration

--- blur.py
+++ blur.py
@@ -3,13 +3,63 @@ import shutil
 import cv2
 import sys
 import os
+from imgurpython import ImgurClient 
+import lib.config 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def laplacian_variance(img):
     return cv2.Laplacian(img, cv2.CV_64F).var()
 
-src = sys.argv[1]
+print("Name of the folder in the same directory as this script?")
+src = input()
 dest = src+'/blurry'
 
+client_id = lib.config.client_id
+client_secret = lib.config.client_secret
+client_user = lib.config.client_user
+client_password = lib.config.client_password
+client = ImgurClient(client_id, client_secret)
+
+
+auth_url = (client.get_auth_url('pin'))
+print(auth_url)
+driver = webdriver.Chrome()
+driver.get(auth_url)
+
+user = driver.find_element_by_xpath('//*[@id="username"]')
+user.clear()
+password = driver.find_element_by_xpath('//*[@id="password"]')
+password.clear()
+user.send_keys(client_user)
+password.send_keys(client_password)
+
+driver.find_element_by_name("allow").click()
+try:
+    pinNum_pres = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "pin"))
+    )
+    pinNum = driver.find_element_by_name("pin").get_attribute("value")
+    print(pinNum)
+except:
+    print("Timed out")
+
+creds = client.authorize(pinNum)
+client.set_user_auth(creds['access_token'], creds['refresh_token'])
+print("Authorized")
+
+albumConfig = {
+    'title': src,
+    'description': 'No blurry pictures'
+}
+album = client.create_album(albumConfig)
+img_cfg = {
+    'album': album['id']
+}
 for path in paths.list_images(src):
     print(path)
     image = cv2.imread(path)
@@ -18,13 +68,15 @@ for path in paths.list_images(src):
 
     #not blurry 
     if image_var > 100:
-         cv2.putText(image, "Not Blurry: {:.2f}".format(image_var), (0,20), cv2.FONT_HERSHEY_PLAIN, 1.50, (0,255,0), 2)
-    #blurry
+        #upload to imgur album here, otherwise move to folder
+        #cv2.putText(image, "Not Blurry: {:.2f}".format(image_var), (0,20), cv2.FONT_HERSHEY_PLAIN, 1.50, (0,255,0), 2)
+        #blurry
+        print("Still need to implement adding to photo on imgur")
     else:
         #need to move the image to another directory
         if not os.path.exists(dest):
             os.makedirs(dest)
-        cv2.putText(image, "Blurry: {:.2f}".format(image_var), (0,20), cv2.FONT_HERSHEY_PLAIN, 1.50, (0,0,255), 2)
+        #cv2.putText(image, "Blurry: {:.2f}".format(image_var), (0,20), cv2.FONT_HERSHEY_PLAIN, 1.50, (0,0,255), 2)
         
         #handle duplicate copy
         try:
