1ef420306ff5bf6d43854694c544cc1e6e9c17a3
Steven Lay <stlay@ucsd.edu>
2017-11-18 21:33:29 -0800

    implemented imgur upload

--- blur.py
+++ blur.py
@@ -27,36 +27,41 @@ client = ImgurClient(client_id, client_secret)
 
 
 auth_url = (client.get_auth_url('pin'))
+print("Click to authorize Imgur Account.")
 print(auth_url)
-driver = webdriver.Chrome()
-driver.get(auth_url)
+#driver = webdriver.Chrome()
+#driver.get(auth_url)
 
-user = driver.find_element_by_xpath('//*[@id="username"]')
-user.clear()
-password = driver.find_element_by_xpath('//*[@id="password"]')
-password.clear()
-user.send_keys(client_user)
-password.send_keys(client_password)
+#user = driver.find_element_by_xpath('//*[@id="username"]')
+#user.clear()
+#password = driver.find_element_by_xpath('//*[@id="password"]')
+#password.clear()
+#user.send_keys(client_user)
+#password.send_keys(client_password)
 
-driver.find_element_by_name("allow").click()
-try:
-    pinNum_pres = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "pin"))
-    )
-    pinNum = driver.find_element_by_name("pin").get_attribute("value")
-    print(pinNum)
-except:
-    print("Timed out")
+#driver.find_element_by_name("allow").click()
+#try:
+#    pinNum_pres = WebDriverWait(driver, 10).until(
+#        EC.presence_of_element_located((By.ID, "pin"))
+#    )
+#    pinNum = driver.find_element_by_name("pin").get_attribute("value")
+#    print(pinNum)
+#except:
+#    print("Timed out")
 
-creds = client.authorize(pinNum)
+creds = client.authorize(input("Enter Pin Number here: "))
 client.set_user_auth(creds['access_token'], creds['refresh_token'])
 print("Authorized")
 
+#Create the album
 albumConfig = {
     'title': src,
     'description': 'No blurry pictures'
 }
+
 album = client.create_album(albumConfig)
+print("Alum ID: " + album['id'])
+album_link = 'https://imgur.com/a/' + album['id']
 img_cfg = {
     'album': album['id']
 }
@@ -71,7 +76,7 @@ for path in paths.list_images(src):
         #upload to imgur album here, otherwise move to folder
         #cv2.putText(image, "Not Blurry: {:.2f}".format(image_var), (0,20), cv2.FONT_HERSHEY_PLAIN, 1.50, (0,255,0), 2)
         #blurry
-        print("Still need to implement adding to photo on imgur")
+        image = client.upload_from_path(path, config=img_cfg, anon=False)
     else:
         #need to move the image to another directory
         if not os.path.exists(dest):
@@ -84,5 +89,7 @@ for path in paths.list_images(src):
             os.remove(path)
         except shutil.SameFileError:
             pass
-    cv2.imshow("Picture", image)
-    key = cv2.waitKey(0)
+print("Done")
+print("Link to the album" + album_link);
+
+#driver.get(album_link);
